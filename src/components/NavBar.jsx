64b50c708febcd3945eb56d0a4954521c2d79cfe
gsancineto <jgabrielsancineto@gmail.com>
2022-01-11 17:14:25 -0300

    navbar menu ecommerce

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { Navbar } from "react-bootstrap"
+import { Container } from "react-bootstrap"
+import { Nav } from "react-bootstrap"
+
+export default function NavBar(){
+    return(
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home" >Mundo Comic</Navbar.Brand>
+                <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#marvel">Marvel</Nav.Link>
+                <Nav.Link href="#dc">DC</Nav.Link>
+                <Nav.Link href="#otros">Otros</Nav.Link>
+                <Nav.Link href="#manga">Manga</Nav.Link>
+                <Nav.Link href="#carrito"><i class="bi bi-cart-fill"/> Carrito</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
