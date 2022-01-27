60be94d61633f32e5649d922246c659561486dd4
gsancineto <jgabrielsancineto@gmail.com>
2022-01-27 19:35:16 -0300

    correccion visual

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -3,21 +3,21 @@ export default function ItemDetail({item}){
 
     return (
         <div>
-            <div class="container">
-                <div class="card">
-                    <div class="container-fliud">
-                        <div class="wrapper row">
-                            <div class="preview col-md-6">
+            <div className="container">
+                <div className="card">
+                    <div className="container-fliud">
+                        <div className="wrapper row">
+                            <div className="preview col-md-6">
                                 
-                                <div class="preview-pic tab-content">
-                                <div class="tab-pane active" id="pic-1"><img src={item.img} width="50%" height="50%"/></div>
+                                <div className="preview-pic tab-content">
+                                <div className="tab-pane active" id="pic-1"><img src={item.img} width="50%" height="50%"/></div>
                                 </div>
                                 
                             </div>
-                            <div class="details col-md-6">
-                                <h3 class="product-title">{item.title}</h3>
-                                <p class="product-description">{item.descLong}</p>
-                                <h4 class="price">Precio: <span>${item.price}</span></h4>
+                            <div className="details col-md-6">
+                                <h3 className="product-title">{item.title}</h3>
+                                <p className="product-description">{item.descLong}</p>
+                                <h4 className="price">Precio: <span>${item.price}</span></h4>
                                 <ItemCount stock={item.stock} initial={item.initial}/>
                             </div>
                         </div>
