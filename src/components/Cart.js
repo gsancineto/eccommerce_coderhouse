1514e67754bb80a8f1457b73cfa2d58ff9c9912a
gsancineto <jgabrielsancineto@gmail.com>
2022-02-12 11:59:51 -0300

    primera parte finalizar compra

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,59 @@
 import { Link } from "react-router-dom";
 import { UseCartContext } from "../context/CartContext"
 import CartDetail from "./CartDetail";
+import { getFirestore, collection, addDoc, where, documentId, writeBatch, getDocs, query } from "firebase/firestore"
 
 export default function Cart(){
-const {cartList} = UseCartContext();
+const {cartList, Total, EmptyCart} = UseCartContext();
+const Buy = async(e) => {
+    // e.preventDefault();
+
+    let order = {}
+
+    order.buyer = {name:"juan", email:"j@gmail.com", phone:"44448888"};
+    order.total = Total();
+
+    order.items = cartList.map(cart => {
+        const id = cart.item.id;
+        const title = cart.item.title;
+        const price = cart.item.price * cart.qty;
+        const qty = cart.qty;
+
+        return {
+            id,
+            title,
+            price,
+            qty
+        }
+    });
+    const db = getFirestore();
+    console.log(order)
+    //comprar
+    const ordersCollection = collection(db,'orders');
+    await addDoc(ordersCollection, order)
+    .then(resp => console.log(resp));
+
+    //actualizar stock
+    const itemsCollection = collection(db,'items');
+    const queryUpdateStock = query(
+        itemsCollection,
+        where(documentId(),'in',cartList.map(cart=>cart.item.id)));
+
+    const batch = writeBatch(db);
+
+    await getDocs(queryUpdateStock)
+    .then(resp=> resp.docs.forEach(res => batch.update(res.ref,{
+        stock:res.data().stock - cartList.find(cart => cart.item.id === res.id).qty
+    })))
+    .finally(console.log('ok'));
+    batch.commit();
+    EmptyCart();
+}
     return(
         <div>
             {
                 cartList.length !== 0 ?
-                <CartDetail />
+                <CartDetail Buy={Buy}/>
                 :
                 <div className="p-3 mb-2 bg-danger text-white d-flex flex-column">
                     No hay productos, vaya a comprar algo 
--- src/components/CartDetail.js
+++ src/components/CartDetail.js
@@ -1,7 +1,7 @@
 import { UseCartContext } from "../context/CartContext";
 
 
-export default function CartDetail(){
+export default function CartDetail({Buy}){
     const {cartList, EmptyCart, Total, DeleteFromCart} = UseCartContext();
     return(
         <>
@@ -11,37 +11,38 @@ export default function CartDetail(){
                         cartList.map(prod => 
                         <li className="list-group-item" key={prod.item.id}> 
                             <div className="card mb-3" style={{width: "100%"}}>
-                                <div className="row no-gutters">
-                                    <div className="col-md-1">
-                                        <img src={prod.item.img} className="card-img" style={{maxWidth:"100px"}}/>
+                                <div className="row no-gutters align-items-center">
+                                    <div className="col-md">
+                                        <img src={prod.item.img} className="card-img"/>
                                     </div>
-                                    <div className="col-md-1">
+                                    <div className="col-md">
                                         <h2 className="card-title text-muted">x{prod.qty}</h2>
                                     </div>
                                     <div className="col-md-4">
                                         <div className="card-body">
                                             <h5 className="card-title">{prod.item.title}</h5>
-                                            <p className="card-text">Precio de lista: ${prod.item.price}</p>
+                                            <p className="card-text">Precio unitario: ${prod.item.price}</p>
                                         </div>
                                     </div>
                                     <div className="col-md-2">
                                         <div className="card-body">
-                                            Total: ${prod.item.price*prod.qty}
+                                        <p className="card-text">Precio: ${prod.item.price*prod.qty}</p>
                                         </div>
                                     </div>
                                     <div className="col-md-4">
-                                    <button onClick={() => DeleteFromCart(prod.item.id)} className="btn btn-danger">Eliminar</button>
+                                        <button onClick={() => DeleteFromCart(prod.item.id)} className="btn btn-danger">Remover</button>
                                     </div>
                                 </div>
                             </div>
                         </li>)
                     }
                 </ul>
-                <div className="float-right">
-                    {`Total: $${Total()}`}
-                </div>
+            </div>
+            <div className="h4 text-light bg-success float-right">
+                {`Total: $${Total()}`}
             </div>
             <button onClick={EmptyCart} className="btn btn-danger">Vaciar Carrito</button>
+            <button onClick={() => Buy()} className="btn btn-success">Finalizar Compra</button>
         </>
     )
 }
